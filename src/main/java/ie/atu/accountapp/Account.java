38ea71e87a4c6ae21ae578566e3909b08c4d9c48
chuka <g00414637@atu.ie>
2025-01-08 22:32:59 +0000

    finished account microservice

--- src/main/java/ie/atu/accountapp/Account.java
+++ src/main/java/ie/atu/accountapp/Account.java
@@ -1,4 +1,28 @@
 package ie.atu.accountapp;
 
-public class Account {
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+class Account {
+    @Id
+    private String accountId;
+    private Double balance;
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
 }
--- src/main/java/ie/atu/accountapp/AccountRepository.java
+++ src/main/java/ie/atu/accountapp/AccountRepository.java
@@ -1,4 +1,7 @@
 package ie.atu.accountapp;
 
-public interface AccountRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface AccountRepository extends JpaRepository<Account, String> {
+
 }
